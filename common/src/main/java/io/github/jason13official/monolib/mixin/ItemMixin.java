af797ec0eed02bb82601b89468a65c10d302a782
jason13official <jason13official@gmail.com>
2026-04-14 00:36:45 -0400

    item mixin to log application

--- common/src/main/java/io/github/jason13official/monolib/mixin/ItemMixin.java
+++ common/src/main/java/io/github/jason13official/monolib/mixin/ItemMixin.java
@@ -1,8 +1,6 @@
 package io.github.jason13official.monolib.mixin;
 
 import io.github.jason13official.monolib.Constants;
-import io.github.jason13official.monolib.platform.Services;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +13,6 @@ public class ItemMixin {
   @Inject(at = @At("TAIL"), method = "<clinit>")
   private static void init(CallbackInfo info) {
 
-    if (Services.PLATFORM.isDevelopmentEnvironment()) {
-      Constants.LOG.info("This line is printed by an example mixin from Common!");
-    }
+    Constants.LOG.info("Mixins applied to net.minecraft.world.item.Item");
   }
 }
