135501b3a911dada30f94e7b9df32085f7b46b24
jason13official <jason13official@gmail.com>
2026-04-16 22:46:49 -0400

    don't verify by default (dependencies need fixing)

--- common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
+++ common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
@@ -2,6 +2,6 @@ package io.github.jason13official.monolib.impl.common;
 
 public class ModConfig {
 
-  public boolean verifyModFilenames = true;
+  public boolean verifyModFilenames = false; // TODO set to true by default at some point in the future when all mods use updated Sailing API lol
   public boolean additionalDebugLogs = false;
 }
