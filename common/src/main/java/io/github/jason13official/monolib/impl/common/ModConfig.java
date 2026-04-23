09f4fa18fa93fc1e8b722a0c1a57d2de90eaab4e
jason13official <jason13official@gmail.com>
2026-04-23 16:11:34 -0400

    no JAR verification by default

--- common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
+++ common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
@@ -1,6 +1,6 @@
 package io.github.jason13official.monolib.impl.common;
 
 public class ModConfig {
-  public boolean verifyModFilenames = false;
+  public boolean verifyModFilenames = false; // 4.0.2, changed to false by default in all versions
   public boolean additionalDebugLogs = false;
 }
