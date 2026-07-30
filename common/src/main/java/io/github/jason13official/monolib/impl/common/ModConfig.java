92ea5b9292460747cf5f4a4e55e1850f3f8af825
jason13official <jason13official@gmail.com>
2026-07-30 13:23:00 -0400

    deprecate old config

--- common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
+++ common/src/main/java/io/github/jason13official/monolib/impl/common/ModConfig.java
@@ -1,5 +1,6 @@
 package io.github.jason13official.monolib.impl.common;
 
+@Deprecated(since = "4.1.0", forRemoval = true)
 public class ModConfig {
 
   public boolean verifyModFilenames = false; // 4.0.2, changed to false by default in all versions
