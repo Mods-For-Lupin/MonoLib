0d0c288ca8af564583576140dc90105fb6f6a354
jason13official <jason13official@gmail.com>
2026-07-30 23:48:54 -0400

    26.1.2 patch: setId dev item

--- common/src/main/java/io/github/jason13official/monolib/impl/common/registry/ModItems.java
+++ common/src/main/java/io/github/jason13official/monolib/impl/common/registry/ModItems.java
@@ -3,7 +3,9 @@ package io.github.jason13official.monolib.impl.common.registry;
 import io.github.jason13official.monolib.MonoLib;
 import io.github.jason13official.monolib.platform.Services;
 import java.util.function.BiConsumer;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
@@ -13,7 +15,7 @@ public class ModItems {
   public static void register(BiConsumer<Item, Identifier> consumer) {
 
     if (Services.PLATFORM.isDevelopmentEnvironment()) {
-      consumer.accept(Services.registry().createSpawnEgg(EntityType.COW, 0x000000, 0xFFFFFF, new Properties()), MonoLib.identifier("safe_service_egg"));
+      consumer.accept(Services.registry().createSpawnEgg(EntityType.COW, 0x000000, 0xFFFFFF, new Properties().setId(ResourceKey.create(Registries.ITEM, MonoLib.identifier("safe_service_egg")))), MonoLib.identifier("safe_service_egg"));
     }
   }
 }
